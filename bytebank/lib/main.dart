744b5b8862dfc5e2ce869af67406732868dd7af0
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-07-23 16:30:11 -0300

    Implementando o design do item de transferência

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -4,7 +4,27 @@ import 'package:flutter/widgets.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
-    appBar: AppBar(title: Text('Tranferências'),),
+    body: Column(
+      children: [
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('100.00'),
+            subtitle: Text('1000'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text('2500.00'),
+            subtitle: Text('2000'),
+          ),
+        ),
+      ],
+    ),
+    appBar: AppBar(
+      title: Text('Tranferências'),
+    ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {  },
       child: Icon(Icons.add),
