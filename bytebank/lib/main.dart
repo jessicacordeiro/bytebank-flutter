39106f551ada3ba2d79e9eb3dae0c40a4bf9032c
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-07-23 16:14:22 -0300

    Utilizando o Material Components

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -2,33 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-void main() => runApp(
-    Column(
-      children: <Widget>[
-        Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Text(
-          'Hello World my name',
-          textDirection: TextDirection.ltr,
-        ),
-        Text (
-          'Olaaaa',
-          textDirection: TextDirection.ltr,
-        ),
-            Column(
-              children: <Widget>[
-                Text (
-                  'Olllllllla',
-                  textDirection: TextDirection.ltr,
-                ),
-              ],
-            ),
-        ],
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(title: Text('Tranferências'),),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      child: Icon(Icons.add),
     ),
-);
+  ),
+));
