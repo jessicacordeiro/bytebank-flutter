282265d11fc29cd56b080df790289914744cd991
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-07-28 12:08:32 -0300

    Estruturando o projeto, seguindo a doc do Flutter Community.

---
+++ bytebank/lib/screens/transferencia/formulario.dart
@@ -0,0 +1,64 @@
+import 'package:bytebank/components/editor.dart';
+import 'package:bytebank/models/transferencia.dart';
+import 'package:flutter/material.dart';
+
+const _tituloAppBar = 'Criando Transferência';
+
+const _rotuloCampoValor = 'Valor';
+const _dicaCampoValor = '0.00';
+
+const _dicaNumeroConta = '0000';
+const _rotuloNumeroConta = 'Número da Conta';
+
+const _textoBotaoConfirmar = 'Confirmar';
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+  TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(_tituloAppBar),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Editor(
+                controlador: _controladorCampoNumeroConta,
+                rotulo: _rotuloNumeroConta,
+                dica: _dicaNumeroConta,
+              ),
+              Editor(
+                controlador: _controladorCampoValor,
+                rotulo: _rotuloCampoValor,
+                dica: _dicaCampoValor,
+                icone: Icons.monetization_on,
+              ),
+              ElevatedButton(
+                onPressed: () => _criaTransferencia(context),
+                child: Text(_textoBotaoConfirmar),
+              ),
+            ],
+          ),
+        ),
+     );
+  }
+
+  void _criaTransferencia(BuildContext context) {
+    final int? numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+    final double? valor = double.tryParse(_controladorCampoValor.text);
+
+    if (numeroConta != null && valor != null) {
+      final transferenciaCriada = Transferencia(valor, numeroConta);
+      Navigator.pop(context, transferenciaCriada);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Transferência Realizada com Sucesso!'),
+        ),
+      );
+    }
+  }
+}
