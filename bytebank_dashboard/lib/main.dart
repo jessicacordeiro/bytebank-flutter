7b449a5c642acf05ff958e699b8d5ffe5b868934
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-08-03 12:06:35 -0300

    Adicionando imagem e botão de contatos

--- bytebank_dashboard/lib/main.dart
+++ bytebank_dashboard/lib/main.dart
@@ -3,19 +3,34 @@ import 'package:flutter/material.dart';
 void main() => runApp(BytebankDashboardApp());
 
 class BytebankDashboardApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold (
-        appBar: AppBar(title: Text('Dashboard'),
-        ),
-        body: Column(
-          children: <Widget> [
-            Image.network('https://i.imgur.com/HMo3p2t.png')
-          ]
-        )
+      theme: ThemeData(
+        primaryColor: Colors.orange[800],
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: TextButton.styleFrom(
+          backgroundColor: Colors.blue[900],
+        )),
       ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('ByteBank'),
+          ),
+          body: Column(children: <Widget>[
+            Image.asset('images/logo-people.png'),
+            Container(
+              height: 120,
+              width: 100,
+              color: Colors.blue[900],
+              child: Column(
+                children: <Widget>[
+                  Icon(Icons.people), 
+                  Text('Contatos')
+                ],
+              ),
+            )
+          ])),
     );
   }
 }
