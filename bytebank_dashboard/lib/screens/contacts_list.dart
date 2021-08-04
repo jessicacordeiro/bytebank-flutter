29d922f769af5733314e6299649fcaa653cacf37
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-08-04 13:56:59 -0300

    Configurando o builder do ListView

--- bytebank_dashboard/lib/screens/contacts_list.dart
+++ bytebank_dashboard/lib/screens/contacts_list.dart
@@ -1,7 +1,11 @@
+import 'package:bytebank_dashboard/models/contact.dart';
 import 'package:bytebank_dashboard/screens/contacts_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+  
+  final List<Contact> contacts = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,25 +17,12 @@ class ContactsList extends StatelessWidget {
           ),
         ),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text(
-                'Jessica',
-                style: TextStyle(
-                  fontSize: 24.0,
-                ),
-              ),
-              subtitle: Text(
-                '0062',
-                style: TextStyle(
-                  fontSize: 20.0,
-                ),
-              ),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final Contact contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -47,6 +38,32 @@ class ContactsList extends StatelessWidget {
         },
         child: Icon(
           Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.nome,
+          style: TextStyle(
+            fontSize: 24.0,
+          ),
+        ),
+        subtitle: Text(
+          contact.numeroConta.toString(),
+          style: TextStyle(
+            fontSize: 20.0,
+          ),
         ),
       ),
     );
