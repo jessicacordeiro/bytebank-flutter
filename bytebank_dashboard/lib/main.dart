9871d42365ac3fcb03bb6979f3d06789e93c5bd8
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-08-04 14:41:38 -0300

    Evitando problemas da execução inicial do FutureBuilder

--- bytebank_dashboard/lib/main.dart
+++ bytebank_dashboard/lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:bytebank_dashboard/database/app_database.dart';
 import 'package:bytebank_dashboard/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(BytebankDashboardApp());
-  findAll().then((contacts) => debugPrint(contacts.toString()));
 }
 
 class BytebankDashboardApp extends StatelessWidget {
