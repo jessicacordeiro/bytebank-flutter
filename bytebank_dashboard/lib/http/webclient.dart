942988ac3a38baef3686cfe16d2cd2bf578fffc2
Jéssica Cordeiro <jessica.cordeiro121@gmail.com>
2021-08-16 15:23:35 -0300

    COnfigurando o logging interceptador

--- bytebank_dashboard/lib/http/webclient.dart
+++ bytebank_dashboard/lib/http/webclient.dart
@@ -1,7 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    debugPrint('Request');
+    debugPrint('url: ${data.url}');
+    debugPrint('headers: ${data.headers}');
+    debugPrint('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    debugPrint('Response');
+    debugPrint('status code: ${data.statusCode}');
+    debugPrint('headers: ${data.headers}');
+    debugPrint('body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
+  Client client = InterceptedClient.build(interceptors: [
+    LoggingInterceptor(),
+  ]);
   final Response response =
-      await get(Uri.http('http://192.168.0.255:8080', 'transactions'));
-  print(response.body);
+      await get(Uri.http('192.168.15.18:8080', 'transactions'));
+  debugPrint(response.body);
 }
